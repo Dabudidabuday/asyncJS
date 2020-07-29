5ba1399f635a428cd97f38649a1b7341c4a83d7f
Myroslav <94mir.ms@gmail.com>
2020-07-29 08:19:04 +0300

    collect form data in progress

--- main.js
+++ main.js
@@ -1,22 +1,50 @@
 'use strict';
 
 let form = document.querySelector('#form');
-let data = new FormData(form);
 
-document.addEventListener('submit', function(event) {
+let getCatalog = new Promise (
+
+        function (resolve, reject) {
+            let data = fetch('https://vape-shop.herokuapp.com/catalog')
+                .then(data => {
+                    data.json()
+                    console.log(data)
+                });
+            resolve(data)
+        }
+)
+
+console.log(getCatalog);
+
+form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    fetch('http://api-atmo.tk/catalog', {
+    let data = {
+        brand: String(event.target.brand.value),
+        title: String(event.target.title.value),
+        price: Number(event.target.price.value),
+        rating: Number(event.target.rating.value),
+        availability: Boolean(event.target.availability.checked)
+    };
+
+    console.log(data)
+
+    fetch('https://vape-shop.herokuapp.com/catalog')
+
+    fetch('https://vape-shop.herokuapp.com/admin/products/add', {
         method: 'POST',
-        body: new FormData(event.target)
+        body: JSON.stringify(data)
     }).then(function(response) {
         if(response.ok) {
             return response.json()
+            console.log(response)
         }
         return Promise.reject(response)
     }).then(function(data) {
         console.log(data)
     }).catch(function(err) {
         console.warn(err)
+
     })
 })
+
